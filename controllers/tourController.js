213650cd56a81582a3114318f7942c2401b17c0c
Jose Gabriel Companioni Benitez <53531665+elC0mpa@users.noreply.github.com>
2021-07-12 13:17:17 -0400

    Created controllers

---
+++ controllers/tourController.js
@@ -0,0 +1,42 @@
+const getAllTours = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'Not implemented route'
+    })
+}
+
+const getTour = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'Not implemented route'
+    })
+}
+
+const createTour = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'Not implemented route'
+    })
+}
+
+const updateTour = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'Not implemented route'
+    })
+}
+
+const deleteTour = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'Not implemented route'
+    })
+}
+
+module.exports = {
+    getAllTours,
+    getTour,
+    updateTour,
+    deleteTour,
+    createTour
+}
---
+++ controllers/userController.js
@@ -0,0 +1,42 @@
+const getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'Not implemented route'
+    })
+}
+
+const getUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'Not implemented route'
+    })
+}
+
+const createUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'Not implemented route'
+    })
+}
+
+const updateUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'Not implemented route'
+    })
+}
+
+const deleteUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'Not implemented route'
+    })
+}
+
+module.exports = {
+    getAllUsers,
+    getUser,
+    updateUser,
+    deleteUser,
+    createUser
+}
