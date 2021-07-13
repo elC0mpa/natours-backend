5b3fa29a237520b22f248d34b130dfa627b39191
Jose Gabriel Companioni Benitez <53531665+elC0mpa@users.noreply.github.com>
2021-07-13 14:13:43 -0400

    Installed dotenv package to load environment variables

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const dotenv = require('dotenv');
 const app = require('./utils/express');
 const db = require('./utils/db');
 
+dotenv.config();
 db(process.env.mongo_url);
 
 app.listen(3000, () => {
