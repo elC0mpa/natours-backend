402457274ea704fa1b2738b9d35c46489b3e9e15
Jose Gabriel Companioni Benitez <53531665+elC0mpa@users.noreply.github.com>
2021-07-13 14:13:55 -0400

    Updated database file

--- utils/db.js
+++ utils/db.js
@@ -1,16 +1,17 @@
-const db = require('mongoose');
+const mongoose = require('mongoose');
 
-db.Promise = global.Promise;
+mongoose.Promise = global.Promise;
 
 function connect(url) {
-  db.connect(url, {
-    useNewUrlParser: true,
-  })
+  mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+    })
     .then(() => {
       console.log('Connected to the Database');
     })
     .catch((error) => {
-      console.error(`Error trying to connect to the DB: ${error}`);
+      console.error(`Error trying to connect to the DB: ${error}\nURL: ${url}`);
     });
 }
 
