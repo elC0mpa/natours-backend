81c23dbe0be7ead97704300bab6cdb0be71c35dc
Jose Gabriel Companioni Benitez <53531665+elC0mpa@users.noreply.github.com>
2021-07-13 17:11:00 -0400

    Updated deleteTour response

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -72,12 +72,9 @@ const updateTour = async (req, res) => {
 
 const deleteTour = async (req, res) => {
   try {
-    const tour = await Tour.findByIdAndDelete(req.params.id);
-    res.status(200).json({
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
       status: 'sucess',
-      data: {
-        tour,
-      },
     });
   } catch (error) {
     res.status(500).json({
