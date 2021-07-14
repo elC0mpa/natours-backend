e4a2bf332a948a30171cf121d3b5d449b74e7cff
Jose Gabriel Companioni Benitez <53531665+elC0mpa@users.noreply.github.com>
2021-07-14 15:44:17 -0400

    Created alias route

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,12 @@
 const Tour = require('../models/tourModel');
 
+const aliasTopTours = async (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 const getAllTours = async (req, res) => {
   try {
     // Filtering
@@ -129,4 +136,5 @@ module.exports = {
   updateTour,
   deleteTour,
   createTour,
+  aliasTopTours,
 };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,11 +6,13 @@ const {
   createTour,
   deleteTour,
   updateTour,
+  aliasTopTours,
 } = require('../controllers/tourController');
 
 const router = express.Router();
 
 router.route('/').get(getAllTours).post(createTour);
+router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
