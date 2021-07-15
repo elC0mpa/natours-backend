4d4c1a66b7b85399dbc6f9fa0756a4a23be4e8ff
Jose Gabriel Companioni Benitez <53531665+elC0mpa@users.noreply.github.com>
2021-07-15 14:37:32 -0400

    Created errorHandler middleware

---
+++ utils/errorHandler.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+const errorHandler = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
+
+module.exports = errorHandler;
