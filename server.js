3fc15e493437b5392fdf22ef9ec0271e7e180190
Jose Gabriel Companioni Benitez <53531665+elC0mpa@users.noreply.github.com>
2021-07-16 10:34:00 -0400

    Changed env variable name

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./utils/express');
 const db = require('./utils/db');
 
 dotenv.config();
-db(process.env.mongo_url);
+db(process.env.MONGO_URL);
 
 app.listen(3000, () => {
   console.log('App running on port 3000');
