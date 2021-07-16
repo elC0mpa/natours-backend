a63c8a25c64e34042593b5c7fa152a126f205713
Jose Gabriel Companioni Benitez <53531665+elC0mpa@users.noreply.github.com>
2021-07-16 12:18:53 -0400

    Added login handler inside authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,12 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const AppError = require('../utils/AppError');
 
+// eslint-disable-next-line no-undef
+const signToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRATION_TIME,
+  });
+
 const signUp = async (req, res, next) => {
   try {
     const newUser = await User.create({
@@ -10,9 +16,9 @@ const signUp = async (req, res, next) => {
       password: req.body.password,
       passwordConfirm: req.body.passwordConfirm,
     });
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET_KEY, {
-      expiresIn: process.env.JWT_EXPIRATION_TIME,
-    });
+
+    const token = signToken(newUser._id);
+
     res.status(201).json({
       status: 'sucess',
       data: {
@@ -25,6 +31,36 @@ const signUp = async (req, res, next) => {
   }
 };
 
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return next(new AppError('Please provide email and password', 400));
+    }
+
+    // Select funtion is necessary because by default password is not read from database
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+      return next(new AppError('Incorrect email or password'), 401);
+    }
+
+    const token = signToken(user._id);
+
+    res.status(201).json({
+      status: 'sucess',
+      data: {
+        token,
+        user,
+      },
+    });
+  } catch (error) {
+    next(new AppError(error.message));
+  }
+};
+
 module.exports = {
   signUp,
+  login,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,11 +7,12 @@ const {
   updateUser,
 } = require('../controllers/userController');
 
-const { signUp } = require('../controllers/authController');
+const { signUp, login } = require('../controllers/authController');
 
 const router = express.Router();
 
 router.route('/signup').post(signUp);
+router.route('/login').post(login);
 router.route('/').get(getAllUsers);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
