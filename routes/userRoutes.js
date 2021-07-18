a114a97a214ad457ef1f0c905025666085d056fc
Jose Gabriel Companioni Benitez <53531665+elC0mpa@users.noreply.github.com>
2021-07-17 22:53:02 -0400

    Added routes to forgotPassword and resetPassword functionalities

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,12 +7,19 @@ const {
   updateUser,
 } = require('../controllers/userController');
 
-const { signUp, login } = require('../controllers/authController');
+const {
+  signUp,
+  login,
+  forgotPassword,
+  resetPassword,
+} = require('../controllers/authController');
 
 const router = express.Router();
 
 router.route('/signup').post(signUp);
 router.route('/login').post(login);
+router.route('/forgot-password').post(forgotPassword);
+router.route('/reset-password/:token').post(resetPassword);
 router.route('/').get(getAllUsers);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
