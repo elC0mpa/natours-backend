97826973114195e4ec7b40bd3f47809c427dc3ab
Jose Gabriel Companioni Benitez <53531665+elC0mpa@users.noreply.github.com>
2021-07-19 09:45:45 -0400

    Created update current user functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,6 +9,44 @@ const getAllUsers = (req, res) => {
   });
 };
 
+const filterObject = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el];
+    }
+  });
+  return newObj;
+};
+
+const updateCurrentUser = async (req, res, next) => {
+  try {
+    // Send error if user sends password data
+    if (req.body.password || req.body.passwordConfirm) {
+      return next(new AppError('This route is not for password updates', 400));
+    }
+
+    // Update user document
+    const filteredBody = filterObject(req.body, 'name', 'email');
+    const updatedUser = await User.findByIdAndUpdate(
+      req.user.id,
+      filteredBody,
+      {
+        new: true,
+        runValidators: true,
+      },
+    );
+    res.status(200).json({
+      status: 'success',
+      data: {
+        user: updatedUser,
+      },
+    });
+  } catch (error) {
+    next(new AppError(error.message), 500);
+  }
+};
+
 const getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
@@ -35,4 +73,5 @@ module.exports = {
   getUser,
   updateUser,
   deleteUser,
+  updateCurrentUser,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,7 @@ const {
   getUser,
   deleteUser,
   updateUser,
+  updateCurrentUser,
 } = require('../controllers/userController');
 
 const {
@@ -23,6 +24,8 @@ router.route('/login').post(login);
 router.route('/forgot-password').post(forgotPassword);
 router.route('/reset-password/:token').patch(resetPassword);
 router.route('/update-password').patch(protectRoute, updatePassword);
+
+router.route('/update-me').patch(protectRoute, updateCurrentUser);
 router.route('/').get(getAllUsers);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
