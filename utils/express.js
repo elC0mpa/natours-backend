b1b8a32e5a6bb329e6d86ed700290f4b40bf64ed
Jose Gabriel Companioni Benitez <53531665+elC0mpa@users.noreply.github.com>
2021-07-19 13:35:34 -0400

    HTTP parameter pollution cleaned

--- utils/express.js
+++ utils/express.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const tourRouter = require('../routes/tourRoutes');
 const userRouter = require('../routes/userRoutes');
@@ -26,6 +27,19 @@ app.use(express.json({ limit: '10kb' }));
 // Data sanitization
 app.use(mongoSanitize());
 app.use(xss());
+
+// Clenaing parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
